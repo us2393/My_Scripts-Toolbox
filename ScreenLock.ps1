f99f1ab00d38e3c35fe003421dbad602bd314d18
us2393 <us2393@hotmail.com>
2022-05-22 10:28:10 +0800

    Update ScreenLock.ps1

--- ScreenLock.ps1
+++ ScreenLock.ps1
@@ -1,3 +1,7 @@
+<# 
+Lock your computer's screen right away.
+Use when you need to be out of your seat for some time. 
+#>
 $xCmdString = {rundll32.exe user32.dll,LockWorkStation}
 
 Invoke-Command $xCmdString
